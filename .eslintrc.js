b342ec7bb1567835188d73e8bec489b3dd4b6837
mrn <mrn.mrzlk@gmail.com>
2022-02-10 12:17:49 +0400

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "extends": "airbnb-base",
+    "rules": {
+    }
+}
